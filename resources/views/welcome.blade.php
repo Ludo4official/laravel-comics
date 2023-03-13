5fc9ddcb4940971dc89a61d3c5dff89d5ccb9eb8
Ludovico Longhini <ludovico.longhini@gmail.com>
2023-03-13 14:44:41 +0100

    laravel comics

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,12 +5,12 @@
     <div class="container">
         <div class="row">
             @foreach ($comics as $comic)
-                <div class="col-2">
+                <div class="col-3">
                     <div class="card" style="width: 18rem;">
                         <img src="https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/81rYQEkagGL._CR0,207,1222,1222_UX256.jpg" class="card-img-top" alt="...">
                         <div class="card-body">
                           <h5 class="card-title">{{ $comic['title']}}</h5>
-                          <p class="card-text">{{ $comic['description']}}</p>
+                          <p class="card-text">{{ $comic['type']}}</p>
                           <p class="card-text">{{ $comic['sale_date']}}</p>
                           <a href="#" class="btn btn-primary">{{ $comic['price']}}</a>
                         </div>
